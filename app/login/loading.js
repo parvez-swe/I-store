5b7586b617b6f83e3f7d61ed1ce068840c571acd
Parvez <mdparvezmusharaf2@gmail.com>
2023-03-30 17:40:10 +0600

    Loading added

--- app/login/loading.js
+++ app/login/loading.js
@@ -1,11 +1,11 @@
+import Loading from "@/components/Loading/Loading";
 import React from "react";
-import { FaSpinner } from "react-icons/fa";
+
 const loading = () => {
   return (
-    <div className="flex items-center justify-center">
-      <FaSpinner className="animate-spin h-8 w-8 text-gray-500" />
-      <span className="ml-2">Loading...</span>
-    </div>
+    <>
+      <Loading />
+    </>
   );
 };
 
--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { FaSpinner } from "react-icons/fa";
-const loading = () => {
+const Loading = () => {
   return (
     <div className="flex items-center justify-center">
-      <FaSpinner className="animate-spin h-8 w-8 text-gray-500" />
+      <FaSpinner className="animate-spin h-8 w-8 text-blue-600" />
       <span className="ml-2">Loading...</span>
     </div>
   );
 };
 
-export default loading;
+export default Loading;
